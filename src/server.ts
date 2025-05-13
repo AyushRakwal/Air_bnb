fc57ce8f36c29b71478c5e59f9f9e7025bb9a787
Ayush Rakwal <ayushrakwal2923@gmail.com>
2025-05-13 19:28:17 +0530

    First

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import sequelize from './db/models/sequelize';
+
 const app = express();
 
 app.use(express.json());
@@ -20,9 +22,11 @@ app.use(appErrorHandler);
 app.use(genericErrorHandler);
 
 
-app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT, async() => {
       logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
       logger.info(`Press Ctrl+C to stop the server.`);
+      await sequelize.authenticate();
+      logger.info(`Database connection established successfully.`);
 });
 
 // Setup := npm install -D typescript ; npx tsc --init
